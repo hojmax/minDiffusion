2a8ebe4deb42bf50a43fa308a6a8d7bf9a190867
hojmax <axelhojmark@gmail.com>
2023-10-03 20:29:06 +0200

    Cleaning

--- mindiffusion/ddpm.py
+++ mindiffusion/ddpm.py
@@ -1,80 +1,6 @@
 from typing import Dict, Tuple
 import torch
 import torch.nn as nn
-from torchvision import transforms
-
-
-class Pixelate:
-    def __init__(self, sizes: list[int], n_between: int = 1):
-        """Sizes is a list of ints from smallest to largest"""
-        self.sizes = sizes
-        self.transforms = [self.set_image_to_random_grey]
-        interpolation = transforms.InterpolationMode.NEAREST
-        for size in sizes:
-            self.transforms.append(
-                transforms.Compose(
-                    [
-                        transforms.Resize(size, interpolation),
-                        transforms.Resize(sizes[-1], interpolation),
-                    ]
-                )
-            )
-        self.n_between = n_between
-        self.n_transforms = len(self.transforms)
-        self.T = self.n_transforms + (self.n_transforms - 1) * self.n_between
-
-    def seed(self, seed: int):
-        torch.manual_seed(seed)
-
-    def get_xT(self):
-        """Returns a random grey image of max size"""
-        size = self.sizes
-        color = torch.rand(1)
-        return torch.ones(1, size, size) * color
-
-    def set_image_to_random_grey(self, image: torch.Tensor):
-        return image * 0 + torch.rand(1)
-
-    def forward(self, image: torch.Tensor, t: int):
-        # t = 0 -> fully pixelated
-        # t = T - 1 -> no pixelation
-        t = self.T - 1 - t
-        fromIndex = t // (self.n_between + 1)
-        interpolation = (t % (self.n_between + 1)) / (self.n_between + 1)
-        fromImage = self.transforms[fromIndex](image)
-        if interpolation == 0:
-            return fromImage
-        else:
-            toIndex = fromIndex + 1
-            toImage = self.transforms[toIndex](image)
-            return (1 - interpolation) * fromImage + interpolation * toImage
-
-
-class DDPMCold(nn.Module):
-    def __init__(
-        self,
-        eps_model: nn.Module,
-        degradation: Pixelate,
-        n_T: int,
-        criterion: nn.Module = nn.MSELoss(),
-    ) -> None:
-        super(DDPMCold, self).__init__()
-        self.degradation = degradation
-        self.eps_model = eps_model
-        self.n_T = n_T
-        self.criterion = criterion
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        _ts = torch.randint(0, self.n_T, (x.shape[0],)).to(x.device)
-        x_t = torch.cat([self.degradation(x, t) for t in _ts], dim=0)
-        return self.criterion(x_t, self.eps_model(x_t, _ts / self.n_T))
-
-    def sample(self, device) -> torch.Tensor:
-        x_t = self.degradation.get_xT().to(device)
-        for s in range(self.n_T - 1, -1, -1):
-            x_0 = self.eps_model(x_t, torch.tensor(s / self.n_T).to(device))
-            x_t = x_t - self.degradation(x_0, s) + self.degradation(x_0, s - 1)
-        return x_t
 
 
 class DDPM(nn.Module):
@@ -112,14 +38,14 @@ class DDPM(nn.Module):
 
         return self.criterion(eps, self.eps_model(x_t, _ts.unsqueeze(1) / self.n_T))
 
-    def sample(self, n_sample: int, size, device) -> torch.Tensor:
-        x_i = torch.randn(n_sample, *size).to(device)  # x_T ~ N(0, 1)
+    def sample(self, n_sample: int, size) -> torch.Tensor:
+        x_i = torch.randn(n_sample, *size).to(self.device)  # x_T ~ N(0, 1)
 
         # This samples accordingly to Algorithm 2. It is exactly the same logic.
         for i in range(self.n_T, 0, -1):
-            z = torch.randn(n_sample, *size).to(device) if i > 1 else 0
+            z = torch.randn(n_sample, *size).to(self.device) if i > 1 else 0
             eps = self.eps_model(
-                x_i, torch.tensor(i / self.n_T).to(device).repeat(n_sample, 1)
+                x_i, torch.tensor(i / self.n_T).to(self.device).repeat(n_sample, 1)
             )
             x_i = (
                 self.oneover_sqrta[i] * (x_i - eps * self.mab_over_sqrtmab[i])
--- mnist.py
+++ mnist.py
@@ -1,44 +1,68 @@
-from typing import Dict, Tuple
 from tqdm import tqdm
 import torch
-import torch.nn as nn
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision import transforms
 from torchvision.utils import save_image, make_grid
 from mindiffusion.unet import UNet
 from mindiffusion.ddpm import DDPM
-import matplotlib.pyplot as plt
 import wandb
 import os
 
 
-def train_mnist(n_epoch: int = 100, device="cuda:0") -> None:
-    wandb.login()
-    wandb.init(project="atia-project", config={}, tags=["mnist"])
-    ddpm = DDPM(eps_model=UNet(stages=3, ctx_sz=1), betas=(1e-4, 0.02), n_T=1000)
-    ddpm.to(device)
-
-    os.makedirs("images", exist_ok=True)
-    os.makedirs("models", exist_ok=True)
-
-    tf = transforms.Compose(
-        [
-            transforms.Resize(32),
-            transforms.ToTensor(),
-        ]
+def get_mnist_dataset():
+    resize_32_tensor = transforms.Compose(
+        [transforms.Resize(32), transforms.ToTensor()]
     )
-
     dataset = MNIST(
         "./data",
         train=True,
         download=True,
-        transform=tf,
+        transform=resize_32_tensor,
     )
+    return dataset
 
-    dataloader = DataLoader(dataset, batch_size=64, shuffle=True, num_workers=2)
-    optim = torch.optim.Adam(ddpm.parameters(), lr=2e-4)
-    for i in range(n_epoch):
+
+def save_sample(ddpm, path):
+    ddpm.eval()
+    with torch.no_grad():
+        xh = ddpm.sample(16, (1, 32, 32))
+        grid = make_grid(xh, nrow=4)
+        save_image(grid, path)
+
+
+def save_model(ddpm, path):
+    torch.save(ddpm.state_dict(), path)
+    wandb.save(path)
+
+
+def train_mnist() -> None:
+    config = {
+        "n_epoch": 100,
+        "device": "cuda:0" if torch.cuda.is_available() else "cpu",
+        "batch_size": 64,
+        "lr": 2e-4,
+        "betas": (1e-4, 0.02),
+        "n_T": 1000,
+        "unet_stages": 3,
+    }
+    wandb.login()
+    wandb.init(project="atia-project", config=config)
+
+    ddpm = DDPM(UNet(config["unet_stages"]), config["betas"], config["n_T"])
+    ddpm.to(config["device"])
+
+    os.makedirs("images", exist_ok=True)
+    os.makedirs("models", exist_ok=True)
+
+    dataloader = DataLoader(
+        get_mnist_dataset(),
+        batch_size=config["batch_size"],
+        shuffle=True,
+        num_workers=2,
+    )
+    optim = torch.optim.Adam(ddpm.parameters(), lr=config["lr"])
+    for i in range(config["n_epoch"]):
         ddpm.train()
         total_loss = 0
 
@@ -46,7 +70,7 @@ def train_mnist(n_epoch: int = 100, device="cuda:0") -> None:
         loss_ema = None
         for x, _ in pbar:
             optim.zero_grad()
-            x = x.to(device)
+            x = x.to(ddpm.device)
             loss = ddpm(x)
             loss.backward()
             total_loss += loss.item()
@@ -60,15 +84,11 @@ def train_mnist(n_epoch: int = 100, device="cuda:0") -> None:
             pbar.set_description(f"loss: {loss_ema:.4f}")
 
         avg_loss = total_loss / len(dataloader)
-        ddpm.eval()
+
         image_save_path = f"images/ddpm_sample_{i}.png"
         model_save_path = f"models/ddpm_mnist_{i}.pth"
-
-        with torch.no_grad():
-            xh = ddpm.sample(16, (1, 32, 32), device)
-            grid = make_grid(xh, nrow=4)
-            save_image(grid, image_save_path)
-            torch.save(ddpm.state_dict(), model_save_path)
+        save_sample(ddpm, image_save_path)
+        save_model(ddpm, model_save_path)
 
         wandb.log(
             {
@@ -77,8 +97,6 @@ def train_mnist(n_epoch: int = 100, device="cuda:0") -> None:
                 f"sample": wandb.Image(image_save_path),
             }
         )
-
-        wandb.save(model_save_path)
 
 
 if __name__ == "__main__":
