89636af23ffaff06a01f8c41c7a3ff0bce233d94
hojmax <axelhojmark@gmail.com>
2023-10-19 14:02:54 +0200

    Update original.py

--- original.py
+++ original.py
@@ -69,8 +69,9 @@ class DDPM(nn.Module):
 
         self.n_T = n_T
         self.criterion = criterion
+        self.test_x = None
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, prediction_index=None) -> torch.Tensor:
         """
         Makes forward diffusion x_t, and tries to guess epsilon value from x_t using eps_model.
         This implements Algorithm 1 in the paper.
@@ -86,24 +87,75 @@ class DDPM(nn.Module):
             + self.sqrtmab[time, None, None, None] * eps
         )  # This is the x_t, which is sqrt(alphabar) x_0 + sqrt(1-alphabar) * eps
         # We should predict the "error term" from this x_t. Loss is what we return.
+        pred = self.eps_model(x_t, time)
 
-        return self.criterion(eps, self.eps_model(x_t, time))
+        if prediction_index is not None:
+            x_t_img = make_image_row(x_t)
+            reconstructed = (
+                x_t - pred * self.sqrtmab[time, None, None, None]
+            ) / self.sqrtab[time, None, None, None]
+            pred_comb_img = make_image_row(
+                torch.clip(
+                    reconstructed,
+                    0,
+                    1,
+                )
+            )
+            pred_img = make_image_row(pred)
+            all = torch.cat([x_t_img, pred_comb_img, pred_img], dim=2)
+            save_image(all, f"images/ddpm{prediction_index}.png")
 
-    def sample(self, n_sample: int, size, device) -> torch.Tensor:
+        return self.criterion(eps, pred)
+
+    def sample(self, n_sample: int, size, device, epoch) -> torch.Tensor:
         x_i = torch.randn(n_sample, *size).to(device)  # x_T ~ N(0, 1)
+        reverse = []
+        predictions = []
 
         # This samples accordingly to Algorithm 2. It is exactly the same logic.
         for i in range(self.n_T, 0, -1):
+            reverse.append(x_i)
             z = torch.randn(n_sample, *size).to(device) if i > 1 else 0
             time = (torch.ones(n_sample) * i).to(device)
             eps = self.eps_model(x_i, time)
+            reconstructed = (x_i - eps * self.sqrtmab[i]) / self.sqrtab[i]
+            predictions.append(reconstructed)
             x_i = (
                 self.oneover_sqrta[i] * (x_i - eps * self.mab_over_sqrtmab[i])
                 + self.sqrt_beta_t[i] * z
             )
+        predictions.append(torch.zeros_like(x_i))
+        reverse.append(x_i)
+
+        test_x = self.test_x[:n_sample]
+        forward = []
+        for i in range(self.n_T, 0, -1):
+            _ts = torch.tensor([i]).repeat(n_sample).to(device)
+            noise = torch.randn_like(test_x)
+            x_t = (
+                self.sqrtab[_ts, None, None, None] * test_x
+                + self.sqrtmab[_ts, None, None, None] * noise
+            )
+            forward.append(x_t)
+        forward.append(test_x)
+
+        reverse_img = torch.cat([make_image_row(x_i) for x_i in reverse], dim=2)
+        forward_img = torch.cat([make_image_row(x_i) for x_i in forward], dim=2)
+        predictions_img = torch.cat([make_image_row(x_i) for x_i in predictions], dim=2)
+        combined = torch.cat([reverse_img, forward_img, predictions_img], dim=3)
+        save_image(combined, f"images/reverse_forward{epoch}.png")
 
         x_i = torch.clamp(x_i, 0, 1)
         return x_i
+
+
+def make_image_row(x: torch.Tensor):
+    """
+    Take tensor (B x C x H x W)
+    And outputs a tensor (1 x C x H x W*B)
+    Where each of the H x W images are placed side by side
+    """
+    return torch.cat(torch.split(x, 1, dim=0), dim=3)
 
 
 def train_mnist(log_wandb) -> None:
@@ -111,12 +163,12 @@ def train_mnist(log_wandb) -> None:
         "resolution": 28,
         "in_channels": 1,
         "out_ch": 1,
-        "ch": 128,
+        "ch": 32,
         "ch_mult": (
             1,
             2,
         ),
-        "num_res_blocks": 3,
+        "num_res_blocks": 1,
         "attn_resolutions": (14,),
         "dropout": 0.1,
     }
@@ -167,7 +219,12 @@ def train_mnist(log_wandb) -> None:
         for x, _ in pbar:
             optim.zero_grad()
             x = x.to(device)
-            loss = ddpm(x)
+            ddpm.test_x = x
+            if total_loss == 0:
+                loss = ddpm(x, i)
+            else:
+                loss = ddpm(x)
+            continue
             loss.backward()
             total_loss += loss.item()
             if loss_ema is None:
@@ -179,7 +236,7 @@ def train_mnist(log_wandb) -> None:
         avg_loss = total_loss / len(dataloader)
         ddpm.eval()
         with torch.no_grad():
-            xh = ddpm.sample(16, (1, 28, 28), device)
+            xh = ddpm.sample(16, (1, 28, 28), device, i)
             grid = make_grid(xh, nrow=4)
             image_path = f"ddpm_sample_{i}.png"
             model_path = f"ddpm_mnist_{i}.pth"
